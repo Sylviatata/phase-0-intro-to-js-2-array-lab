d52923aa7cc2a7c0689cf8b1b596299898f02a6f
Sylvia <sylvia.tata@student.moringaschool.com>
2024-04-05 21:19:42 +0300

    Array Manipulation exercise

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+// const destructivelyAppendCat = cats.push("Ralph");
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+//   console.log(cats);
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+
+  function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+  }
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+}
+
+function appendCat(name) {
+    const newcatsArray = cats.slice();
+    newcatsArray.push(name);
+    return newcatsArray;
+}
+
+function prependCat(name) {
+    const newcats = cats.slice();
+    newcats.unshift(name);
+    return newcats;
+}
+
+function removeLastCat(name) {
+    const lastCatOff = cats.slice(0, -1);
+    return lastCatOff;
+}
+
+function removeFirstCat(name) {
+    const firstCatOff = cats.slice(1);
+    return firstCatOff;
+}
